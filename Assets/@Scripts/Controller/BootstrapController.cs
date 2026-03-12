89d62515988a4a69ebe2f5879e20cb448edcff43
ickim <yainin0119@gmail.com>
2026-03-12 23:30:07 +0900

    Update) 컴포넌트 모듈 스크립트 추가

---
+++ Assets/@Scripts/Controller/BootstrapController.cs
@@ -0,0 +1,24 @@
+using JJORY.Module;
+using UnityEngine;
+
+namespace JJORY.Controller
+{
+    public class BootstrapController : MonoBehaviour
+    {
+        #region Class
+        //class AddressableManager : Sequence
+        //{
+        //
+        //}
+        #endregion
+
+        #region Variable
+        #endregion
+
+        #region LifeCycle
+        #endregion
+
+        #region Method
+        #endregion
+    }
+}
